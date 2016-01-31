c293133053e6a38263a75f66e31882ba0113bab2
Marquis Dennis <marquis103@gmail.com>
2016-01-31 01:34:13 -0500

    Added reverb and echo effects

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -17,21 +17,27 @@ class PlaySoundsViewController: UIViewController {
   var audioPlayer: AVAudioPlayerNode!
   var timePitch: AVAudioUnitTimePitch!
   var selectedButton:UIButton?
+  var reverb:AVAudioUnitReverb!
+  var echo:AVAudioUnitDelay!
   
   override func viewDidLoad() {
     audioEngine = AVAudioEngine()
     
     audioPlayer = AVAudioPlayerNode()
     timePitch = AVAudioUnitTimePitch()
-  
+    reverb = AVAudioUnitReverb()
+    echo = AVAudioUnitDelay()
+    
     audioEngine.attachNode(audioPlayer)
     audioEngine.attachNode(timePitch)
+    audioEngine.attachNode(reverb)
+    audioEngine.attachNode(echo)
     
     audioFile = try! AVAudioFile(forReading: receiveAudio.filePathUrl, commonFormat: .PCMFormatFloat32, interleaved: false)
     
   }
   
-  func startAudioEngine(withRate rate:Float, usingPitch pitch:Float? = 1.0) {
+  func startAudioEngine(withRate rate:Float, usingPitch pitch:Float? = 1.0, reverbValue:Float?, echoValue:NSTimeInterval?) {
     if audioPlayer.playing {
       audioPlayer.stop()
     }
@@ -41,7 +47,22 @@ class PlaySoundsViewController: UIViewController {
     timePitch.pitch = pitch!
     timePitch.rate = rate
     
-    audioEngine.connect(audioPlayer, to: timePitch, format: nil)
+    if let echoValue = echoValue, let reverbValue = reverbValue {
+      reverb.loadFactoryPreset(.LargeHall)
+      reverb.wetDryMix = reverbValue
+      
+      echo.delayTime = echoValue
+
+    } else {
+      reverb.loadFactoryPreset(.MediumHall)
+      reverb.wetDryMix = 0.0
+      
+      echo.delayTime = 0
+    }
+    
+    audioEngine.connect(audioPlayer, to: echo, format: nil)
+    audioEngine.connect(echo, to: reverb, format: nil)
+    audioEngine.connect(reverb, to: timePitch, format: nil)
     audioEngine.connect(timePitch, to: audioEngine.outputNode, format: nil)
     
     audioPlayer.scheduleFile(audioFile, atTime: nil) { () -> Void in
@@ -55,25 +76,31 @@ class PlaySoundsViewController: UIViewController {
   
   @IBAction func playChipmunkAudio(sender: UIButton) {
     selectedButton = sender
-    startAudioEngine(withRate: 1.5, usingPitch: 1000)
+    startAudioEngine(withRate: 1.5, usingPitch: 1000, reverbValue: nil, echoValue: nil)
   }
   
   @IBAction func playFastAudio(sender: UIButton) {
     selectedButton = sender
-    startAudioEngine(withRate: 2.0)
+    startAudioEngine(withRate: 2.0, usingPitch: 1.0, reverbValue: nil, echoValue: nil)
   }
   
   @IBAction func playSlowAudio(sender: UIButton) {
     selectedButton = sender
-    startAudioEngine(withRate: 0.5)
+    startAudioEngine(withRate: 0.5, usingPitch: 1.0, reverbValue: nil, echoValue: nil)
   }
 
   @IBAction func stopAutio(sender: UIButton) {
     audioPlayer.stop()
   }
   
+  
+  @IBAction func playReverbEcho(sender: UIButton) {
+    selectedButton = sender
+    startAudioEngine(withRate: 1.0, usingPitch: 1.0, reverbValue: 50, echoValue: 0.4)
+  }
+  
   @IBAction func playDarthVaderAudio(sender: UIButton) {
     selectedButton = sender
-    startAudioEngine(withRate: 0.7, usingPitch: -1000)
+    startAudioEngine(withRate: 0.7, usingPitch: -1000, reverbValue: nil, echoValue: nil)
   }
 }
